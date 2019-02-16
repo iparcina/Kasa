8caba964afe58d5b57dc1e07ab73a228002a4e6e
Ivana Parcina <ivana_parcina@hotmail.com>
2019-02-16 19:35:22 +0100

    first commit

---
+++ Kasa/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Kasa
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+               name: "GetProducts",
+               routeTemplate: "api/products",
+               defaults: new { controller = "Products", action = "GetProizvodi" }
+               );
+
+            config.Routes.MapHttpRoute(
+               name: "SetProducts",
+               routeTemplate: "api/addproducts",
+               defaults: new { controller = "Products", action = "PostProizvodi" }
+               );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ Kasa/Controllers/ProductsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Kasa.Models;
+
+namespace Kasa.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private proizvodi_Entities db = new proizvodi_Entities();
+
+        // GET: api/Products
+        [HttpGet]
+        public IQueryable<Proizvodi> GetProizvodi()
+        {
+            return db.Proizvodi;
+        }
+
+        // GET: api/Products/5
+
+        [ResponseType(typeof(Proizvodi))]
+        public IHttpActionResult GetProizvodi(int id)
+        {
+            Proizvodi proizvodi = db.Proizvodi.Find(id);
+            if (proizvodi == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(proizvodi);
+        }
+
+        // PUT: api/Products/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProizvodi(int id, Proizvodi proizvodi)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != proizvodi.ID_proizvod)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(proizvodi).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProizvodiExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        [ResponseType(typeof(Proizvodi))]
+        public IHttpActionResult PostProizvodi([FromBody]Proizvodi proizvodi)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Proizvodi.Add(proizvodi);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = proizvodi.ID_proizvod }, proizvodi);
+        }
+
+        // DELETE: api/Products/5
+        [ResponseType(typeof(Proizvodi))]
+        public IHttpActionResult DeleteProizvodi(int id)
+        {
+            Proizvodi proizvodi = db.Proizvodi.Find(id);
+            if (proizvodi == null)
+            {
+                return NotFound();
+            }
+
+            db.Proizvodi.Remove(proizvodi);
+            db.SaveChanges();
+
+            return Ok(proizvodi);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProizvodiExists(int id)
+        {
+            return db.Proizvodi.Count(e => e.ID_proizvod == id) > 0;
+        }
+    }
+}
